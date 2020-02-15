7418b4c104473a8c0bd58a8bbbea87d9af270cc8
John Ferguson Smart <john.smart@wakaleo.com>
2020-02-15 12:31:27 +0530

    Solutions and starter class

--- src/test/java/net/bddtrader/JSONPathAssertionExerciseSolutions.java
+++ src/test/java/net/bddtrader/JSONPathAssertionExerciseSolutions.java
@@ -4,9 +4,12 @@ import io.restassured.RestAssured;
 import org.junit.Before;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.*;
 
 public class JSONPathAssertionExerciseSolutions {
+
 
     @Before
     public void prepare_rest_config() {
@@ -20,22 +23,21 @@ public class JSONPathAssertionExerciseSolutions {
      */
     @Test
     public void find_a_simple_field_value() {
-
-        RestAssured.given().pathParam("symbol","aapl")
-                .when().get("/stock/{symbol}/company")
+        given().pathParam("symbol", "aapl")
+                .when().get("stock/{symbol}/company")
                 .then().body("industry", equalTo("Telecommunications Equipment"));
     }
 
     /**
      * Exercise 2 - Read a single field value
-     * Complete the test shown below to read and verify the "industry" field for AAPL
-     * The result should be 'Telecommunications Equipment'
+     * Complete the test shown below to read and verify the "description" field for AAPL
+     * The result should contain the string 'smartphones'
      */
     @Test
     public void check_that_a_field_value_contains_a_given_string() {
 
-        RestAssured.given().pathParam("symbol","aapl")
-                .when().get("/stock/{symbol}/company")
+        given().pathParam("symbol", "aapl")
+                .when().get("stock/{symbol}/company")
                 .then().body("description", containsString("smartphones"));
     }
 
@@ -47,8 +49,8 @@ public class JSONPathAssertionExerciseSolutions {
      */
     @Test
     public void find_a_nested_field_value() {
-        RestAssured.given().pathParam("symbol","aapl")
-                .when().get("/stock/{symbol}/book")
+        given().pathParam("symbol", "aapl")
+                .when().get("stock/{symbol}/book")
                 .then().body("quote.symbol", equalTo("AAPL"));
     }
 
@@ -60,9 +62,7 @@ public class JSONPathAssertionExerciseSolutions {
      */
     @Test
     public void find_a_list_of_values() {
-
-        RestAssured
-                .when().get("/tops/last")
+        when().get("/tops/last")
                 .then().body("symbol", hasItems("PTN", "PINE", "TRS"));
     }
 
@@ -72,10 +72,8 @@ public class JSONPathAssertionExerciseSolutions {
      */
     @Test
     public void make_sure_at_least_one_item_matches_a_given_condition() {
-
-        RestAssured
-                .when().get("/tops/last")
-                .then().body("price", hasItem(greaterThan(100.0f)));
+        when().get("/tops/last")
+                .then().body("price", hasItems(greaterThan(100.0f)));
     }
 
     /**
@@ -84,8 +82,7 @@ public class JSONPathAssertionExerciseSolutions {
      */
     @Test
     public void find_a_field_of_an_element_in_a_list() {
-
-        RestAssured.given().pathParam("symbol","aapl")
+        given().pathParam("symbol","aapl")
                 .when().get("/stock/{symbol}/book")
                 .then().body("trades[0].price", equalTo(319.59f));
     }
@@ -96,22 +93,20 @@ public class JSONPathAssertionExerciseSolutions {
      */
     @Test
     public void find_a_field_of_the_last_element_in_a_list() {
-
-        RestAssured.given().pathParam("symbol","aapl")
+        given().pathParam("symbol","aapl")
                 .when().get("/stock/{symbol}/book")
                 .then().body("trades[-1].price", equalTo(319.54f));
     }
 
     /**
-     * Exercise 8 - check for minimum or maximum
+     * Exercise 8 - check for number of elements in a collection
      * Check that there are 20 trades
      */
     @Test
     public void find_the_number_of_trades() {
-
-        RestAssured.given().pathParam("symbol","aapl")
+        given().pathParam("symbol","aapl")
                 .when().get("/stock/{symbol}/book")
-                .then().body("trades.price.size()", equalTo(20));
+                .then().body("trades.size()",equalTo(20));
     }
 
     /**
@@ -120,34 +115,32 @@ public class JSONPathAssertionExerciseSolutions {
      */
     @Test
     public void find_the_minimum_trade_price() {
-
-        RestAssured.given().pathParam("symbol","aapl")
+        given().pathParam("symbol","aapl")
                 .when().get("/stock/{symbol}/book")
                 .then().body("trades.price.min()", equalTo(319.38f));
+
     }
 
     /**
-     * Exercise 10 - check for minimum or maximum
-     * Check that the minimum price of any trade in the Apple stock book is 319.38
+     * Exercise 10 - find a value using closure expressions
+     * Check that the volume of the trade with the minimum price is 100
      */
     @Test
     public void find_the_size_of_the_trade_with_the_minimum_trade_price() {
-
-        RestAssured.given().pathParam("symbol","aapl")
+        given().pathParam("symbol","aapl")
                 .when().get("/stock/{symbol}/book")
-                .then().body("trades.min{ t -> t.price }.size", equalTo(100.0f));
+                .then().body("trades.min { it.price }.volume", equalTo(100f));
     }
 
     /**
-     * Exercise 12 - find a collection of objects matching a specified criteria
+     * Exercise 11 - find a collection of objects matching a specified criteria
      * Check that there are 13 trades with prices greater than 319.50
      */
     @Test
-    public void find_the_number_of_trades_with_a_price_greater_than_some_valye() {
-
+    public void find_the_number_of_trades_with_a_price_greater_than_some_value() {
         RestAssured.given().pathParam("symbol","aapl")
                 .when().get("/stock/{symbol}/book")
-                .then().body("trades.findAll{ t -> t.price > 319.50 }.size()", equalTo(13));
+                .then().body("trades.findAll { it.price > 319.50 }.size()", equalTo(13));
     }
 
 }
