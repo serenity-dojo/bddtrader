d34a42dcdd8eefb5ad6e14dbb956e53939abcd4b
John Ferguson Smart <john.smart@wakaleo.com>
2020-11-24 16:58:21 +0000

    Updated lesson 2 exercises

--- src/test/java/net/bddtrader/WhenGettingCompanyNews.java
+++ src/test/java/net/bddtrader/WhenGettingCompanyNews.java
@@ -5,6 +5,9 @@ import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.everyItem;
+
 public class WhenGettingCompanyNews {
 
     @Before
@@ -13,13 +16,19 @@ public class WhenGettingCompanyNews {
     }
 
     @Test
-    public void should_return_name_and_sector() {
-        RestAssured.given()
-                .pathParam("symbol","aapl")
-                .when()
-                .get("https://bddtrader.herokuapp.com/api/stock/{symbol}/company")
+    public void should_return_name_and_sector_from_local() {
+        RestAssured.get("http://localhost:9000/api/stock/aapl/company")
                 .then()
                 .body("companyName", Matchers.equalTo("Apple, Inc."))
                 .body("sector",Matchers.equalTo("Electronic Technology"));
+    }
+
+    @Test
+    public void should_return_news_for_a_requested_company() {
+        RestAssured.given().queryParam("symbols","AAPL")
+                .when()
+                .get("/news")
+                .then()
+                .body("related", everyItem(containsString("AAPL")));
     }
 }
