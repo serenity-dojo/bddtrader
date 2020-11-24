1c04d71e8c29e4c378cfdd8cb556407afe09c2b9
John Ferguson Smart <john.smart@wakaleo.com>
2020-11-24 16:58:21 +0000

    Added an extra exercise

--- src/test/java/net/bddtrader/WhenGettingCompanyNews.java
+++ src/test/java/net/bddtrader/WhenGettingCompanyNews.java
@@ -5,6 +5,9 @@ import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.everyItem;
+
 public class WhenGettingCompanyNews {
 
     @Before
@@ -26,6 +29,15 @@ public class WhenGettingCompanyNews {
                 .then()
                 .body("companyName", Matchers.equalTo("Apple, Inc."))
                 .body("sector",Matchers.equalTo("Electronic Technology"));
+    }
+
+    @Test
+    public void should_return_news_for_a_requested_company() {
+        RestAssured.given().queryParam("symbols","AAPL")
+                .when()
+                .get("/news")
+                .then()
+                .body("related", everyItem(containsString("AAPL")));
     }
 
 }
