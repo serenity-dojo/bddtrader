98c862fc35d913214828b9d05dad81d1fd23dd56
John Ferguson Smart <john.smart@wakaleo.com>
2020-11-24 11:40:19 +0000

    Sample solution to lesson 1

--- src/test/java/net/bddtrader/WhenGettingCompanyDetails.java
+++ src/test/java/net/bddtrader/WhenGettingCompanyDetails.java
@@ -7,17 +7,27 @@ import org.junit.Test;
 
 public class WhenGettingCompanyDetails {
 
-    @Before
-    public void prepare_rest_config() {
-        RestAssured.baseURI = "https://bddtrader.herokuapp.com/api";
-    }
-
     @Test
-    public void should_return_name_and_sector() {
+    public void should_return_name_and_sector_in_production() {
+        RestAssured.baseURI = "https://bddtrader.herokuapp.com/api";
+
         RestAssured.given()
                 .pathParam("symbol","aapl")
                 .when()
-                .get("https://bddtrader.herokuapp.com/api/stock/{symbol}/company")
+                .get("/stock/{symbol}/company")
+                .then()
+                .body("companyName", Matchers.equalTo("Apple, Inc."))
+                .body("sector",Matchers.equalTo("Electronic Technology"));
+    }
+
+    @Test
+    public void should_return_name_and_sector_locally() {
+        RestAssured.baseURI = "http://localhost:9000/api";
+
+        RestAssured.given()
+                .pathParam("symbol","aapl")
+                .when()
+                .get("/stock/{symbol}/company")
                 .then()
                 .body("companyName", Matchers.equalTo("Apple, Inc."))
                 .body("sector",Matchers.equalTo("Electronic Technology"));
